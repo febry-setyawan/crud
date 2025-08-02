e3788c9b50665f0f06f7aaf8ac95b17acae51377
febry-setyawan <83659383+febry-setyawan@users.noreply.github.com>
2025-08-02 11:40:29 +0700

    Fixing : remove hardcode password

--- src/main/java/com/example/crud/config/SecurityConfig.java
+++ src/main/java/com/example/crud/config/SecurityConfig.java
@@ -4,16 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,7 +31,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .httpBasic(withDefaults()) // Aktifkan HTTP Basic Authentication
-            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())); // Izinkan H2 Console di dalam frame
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin)); // Izinkan H2 Console di dalam frame
 
         return http.build();
     }
@@ -35,9 +40,11 @@ public class SecurityConfig {
     public InMemoryUserDetailsManager userDetailsService() {
         // Buat user sederhana di memori untuk testing
         @SuppressWarnings("deprecation")
+        String password = RandomStringUtils.randomAlphanumeric(10);
+        log.debug("Generated password for user: {}", password);
         UserDetails user = User.withDefaultPasswordEncoder()
             .username("user")
-            .password("password")
+            .password(password)
             .roles("USER")
             .build();
         return new InMemoryUserDetailsManager(user);
