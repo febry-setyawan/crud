813bc4964570131d09372063776fb2bda763a5cb
febry-setyawan <83659383+febry-setyawan@users.noreply.github.com>
2025-08-03 19:13:43 +0700

    feature: integrate to spring cloud registry

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -3,10 +3,12 @@ package com.example.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @EnableCaching
 public class CrudApplication {
